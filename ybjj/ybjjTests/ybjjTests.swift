6ab7dac779a6636f1a17960c8897afdcfc850f6c
Seth Daetwiler <sericha@rei.com>
2025-04-21 19:00:15 -0700

    Init

---
+++ ybjj/ybjjTests/ybjjTests.swift
@@ -0,0 +1,17 @@
+//
+//  ybjjTests.swift
+//  ybjjTests
+//
+//  Created by Seth Daetwiler on 4/21/25.
+//
+
+import Testing
+@testable import ybjj
+
+struct ybjjTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
